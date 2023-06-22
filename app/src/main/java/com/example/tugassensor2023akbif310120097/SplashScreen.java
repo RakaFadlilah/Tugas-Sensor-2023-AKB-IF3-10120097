37d87d8f6b38a8cdccf856f66466e630c5871563
RakaFadlilah <raka.10120097@mahasiswa.unikom.ac.id>
2023-06-22 23:37:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugassensor2023akbif310120097/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.tugassensor2023akbif310120097;
+/* NIM: 10120097 Nama : Raka Fadlilah Kelas : IF-3 */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
